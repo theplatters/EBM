124f722740eba240a6dc4d728ba9c193e140c91b
Franz Scharnreitner <fscharnreitner@gmail.com>
2026-04-06 18:38:52 +0000

    new plots from the notebook

--- notebooks/sweeps.jl
+++ notebooks/sweeps.jl
@@ -13,6 +13,9 @@ Pkg.activate("..")
 # ╔═╡ 8cbcc055-ab0c-4fe9-b4c6-82471a51af3c
 using Revise
 
+# ╔═╡ c1543a0a-59c5-4953-855f-1804aa40a8a5
+using DataFrames
+
 # ╔═╡ 21c2750c-2882-11f1-a92f-398891230811
 using EBM, CairoMakie, StatsBase
 
@@ -29,26 +32,35 @@ sweep = Traffic.run_all(resolution = 10, depth=100)
 # ╔═╡ dc041d84-e0b0-483b-9e4f-260901d500ae
 sweep_logger = Dict(k => Traffic.MeanLogger(v) for (k,v) in sweep)
 
-# ╔═╡ 83c40302-6942-4d4c-aeff-57fe888427d0
-Dict( k=> sum(v.deaths) for (k,v) in sweep_logger)
-
-# ╔═╡ 7b12826f-df23-4eca-bcf3-7ff79e962a5f
-sweep
+# ╔═╡ 02de7688-dc32-445f-8b78-9328b873d5bc
+# ╠═╡ show_logs = false
+abm_sweep = Traffic.sweep_weights(Traffic.ABM(), resolution = 10, depth = 100);
 
 # ╔═╡ f4fd8918-37cd-41ec-8e83-fa766d6df671
 mean_age = Dict(k => v.mean_age for (k, v) in sweep_logger)
 
 
+# ╔═╡ e04a625c-f493-4fc1-b3ab-baacc8df6595
+abm_mean_age = combine(groupby(vcat([r[1] for r in abm_sweep]...),:time),:age => mean, :habitus => abs ∘ mean );
+
+# ╔═╡ 0645842c-9a17-49e1-823f-6cd701e32e0b
+generate_label(str) = replace(string(typeof(str)), "EBM.Traffic." => "")
+
 # ╔═╡ c6507f4e-e1c1-4498-b91f-1a805c641175
 begin
 	f = Figure()
-	ax = Axis(f[1,1])
+	ax = Axis(f[1,1], xlabel = "Step", ylabel="Mean age")
 	for (strategy, vals) in mean_age
-        lines!(ax, vals, label = string(typeof(strategy)))
+        lines!(ax,
+			   vals,
+			   label = generate_label(strategy))
     end
-	Legend(f[1, 2], ax)
+	lines!(ax, abm_mean_age.age_mean, label = "ABM", linestyle=:dot)
 
-	f
+	Legend(f[2, 1], ax)
+
+	save("../plots/mean_age.png",f)
+f
 	
 end
 
@@ -60,34 +72,39 @@ mean_habitus = Dict(k => v.mean_abs_habitus for (k, v) in sweep_logger)
 # ╔═╡ 53495d67-9325-4410-929d-30123e0f4f31
 begin
 	f2 = Figure()
-	ax2 = Axis(f2[1,1])
+	ax2 = Axis(f2[1,1], xlabel="Step", ylabel="Mean absolute habitus")
 	for (strategy, vals) in mean_habitus
-        lines!(ax2, vals, label = string(typeof(strategy)))
+        lines!(ax2, vals, label = generate_label(strategy))
     end
-	Legend(f2[1, 2], ax)
+	        lines!(ax2, abm_mean_age.habitus_abs_mean, label = "ABM", linestyle=:dot)
 
+	Legend(f2[1, 2], ax2)
+
+	save("../plots/habitus.png",f2)
 	f2
 	
 end
 
-# ╔═╡ 2d2cff3e-fa94-4803-9afd-d20502bf88df
-# ╠═╡ show_logs = false
- unsure_sweep = Traffic.sweep_weights(strategy=Traffic.UnsureStrategy(), depth=100, resolution=20)
-
-# ╔═╡ 3531b0d5-b88c-4296-a30f-306a77086103
-# ╠═╡ show_logs = false
- mhs_sweep = Traffic.sweep_weights(strategy=Traffic.MeanHabitusStrategy(), depth=100, resolution=20)
-
-# ╔═╡ d79da30c-c57f-438c-9afd-9143754e7274
-# ╠═╡ show_logs = false
-random_sweep = Traffic.sweep_weights(strategy=Traffic.RandomStrategy(), depth=100, resolution=20)
-
-# ╔═╡ bb33eab3-aee4-4142-8608-30ba63a713e2
-# ╠═╡ show_logs = false
-naive_sweep = Traffic.sweep_weights(strategy=Traffic.NaiveStrategy(), depth=100, resolution=20)
+# ╔═╡ b7e47d12-e79a-4869-bd8c-4298aeeeae81
+only_hab = Dict(k => Traffic.MeanLogger(filter(v) do val
+	val.weights.wₕ >= 0.8
+end) for (k,v) in sweep)
 
 # ╔═╡ 6b55052a-8de0-4e43-bc6c-50aaaf0b5297
-score(sw) = -sum(sw.logger.deaths)
+score(sw) = mean(sw.logger.mean_age)
+
+# ╔═╡ 06e73989-be81-4b2b-a03d-e466f9ecdf82
+begin
+	f4 = Figure()
+	ax4 = Axis(f4[1,1])
+	for (strategy, vals) in only_hab
+        lines!(ax4, vals.mean_age, label = string(typeof(strategy)))
+    end
+	Legend(f4[1, 2], ax4)
+
+	f4
+	
+end
 
 # ╔═╡ 2bc614f0-65c4-40cc-8ac6-32061d97ba00
 newdict = Dict(k => argmax(score, v) for (k, v) in sweep)
@@ -106,21 +123,16 @@ begin
 	
 end
 
-# ╔═╡ b7e47d12-e79a-4869-bd8c-4298aeeeae81
-only_hab = Dict(k => Traffic.MeanLogger(filter(v) do val
-	val.weights.wₕ >= 0.7
-end) for (k,v) in sweep)
-
-# ╔═╡ 06e73989-be81-4b2b-a03d-e466f9ecdf82
+# ╔═╡ 7feeff36-fc1d-4928-b59a-32d29b536263
 begin
-	f4 = Figure()
-	ax4 = Axis(f4[1,1])
-	for (strategy, vals) in only_hab
-        lines!(ax4, vals.mean_age, label = string(typeof(strategy)))
+	f5 = Figure()
+	ax5 = Axis(f5[1,1])
+	for (strategy, vals) in sweep_logger
+        lines!(ax5, vals.stay_ratio[5:end], label = string(typeof(strategy)))
     end
-	Legend(f4[1, 2], ax4)
+	Legend(f5[1, 2], ax5)
 
-	f4
+	f5
 	
 end
 
@@ -129,22 +141,21 @@ end
 # ╠═7a45ec5c-eae6-4f8f-b079-f78b48e8f31c
 # ╠═a8931600-799e-400b-aef7-60061a02eba0
 # ╠═8cbcc055-ab0c-4fe9-b4c6-82471a51af3c
+# ╠═c1543a0a-59c5-4953-855f-1804aa40a8a5
 # ╠═21c2750c-2882-11f1-a92f-398891230811
 # ╠═fbb723e4-c656-4f06-8987-8724b0635c12
 # ╠═79e6b7ee-baf0-4d37-b0ec-3e830543b925
 # ╠═dc041d84-e0b0-483b-9e4f-260901d500ae
-# ╠═83c40302-6942-4d4c-aeff-57fe888427d0
-# ╠═7b12826f-df23-4eca-bcf3-7ff79e962a5f
+# ╠═02de7688-dc32-445f-8b78-9328b873d5bc
 # ╠═f4fd8918-37cd-41ec-8e83-fa766d6df671
+# ╠═e04a625c-f493-4fc1-b3ab-baacc8df6595
+# ╠═0645842c-9a17-49e1-823f-6cd701e32e0b
 # ╠═c6507f4e-e1c1-4498-b91f-1a805c641175
-# ╠═a4cbd9d1-81a6-4d68-86a9-623da00ca7ba
 # ╠═53495d67-9325-4410-929d-30123e0f4f31
-# ╠═2d2cff3e-fa94-4803-9afd-d20502bf88df
-# ╠═3531b0d5-b88c-4296-a30f-306a77086103
-# ╠═d79da30c-c57f-438c-9afd-9143754e7274
-# ╠═bb33eab3-aee4-4142-8608-30ba63a713e2
-# ╠═6b55052a-8de0-4e43-bc6c-50aaaf0b5297
+# ╟─a4cbd9d1-81a6-4d68-86a9-623da00ca7ba
+# ╟─b7e47d12-e79a-4869-bd8c-4298aeeeae81
+# ╟─6b55052a-8de0-4e43-bc6c-50aaaf0b5297
+# ╠═06e73989-be81-4b2b-a03d-e466f9ecdf82
 # ╠═2bc614f0-65c4-40cc-8ac6-32061d97ba00
 # ╠═9fcad449-a5b2-4bf1-9b2a-1aa44a6587d9
-# ╠═b7e47d12-e79a-4869-bd8c-4298aeeeae81
-# ╠═06e73989-be81-4b2b-a03d-e466f9ecdf82
+# ╠═7feeff36-fc1d-4928-b59a-32d29b536263
