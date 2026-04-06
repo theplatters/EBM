7ecb29e53d3891af554cab1b9b55dba4e96a44c6
theplatters <fscharnreitner@gmail.com>
2026-04-06 11:21:53 +0200

    more bug fix

--- src/Traffic/systems/habitus.jl
+++ src/Traffic/systems/habitus.jl
@@ -1,11 +1,14 @@
-lane_to_LR(laneIndex::Int) = laneIndex == 1 ? 1.0 : -1.0
+@inline lane_to_LR(laneIndex::Int, dir::Direction) =
+    dir == Clockwise ?
+    (laneIndex == 1 ? 1.0 : -1.0) :
+    (laneIndex == 2 ? 1.0 : -1.0)
 
 function update_habitus!(world)
     params = Ark.get_resource(world, ModelParams)
 
-    for (e, pos, hab, step) in Query(world, (Position, Habitus, Step))
+    for (e, pos, dir, hab, step) in Query(world, (Position, Direction, Habitus, Step))
         @inbounds for i in eachindex(e)
-            new_hab = Habitus(clamp(hab[i].val + lane_to_LR(pos[i].x) / (params.K + step[i].val), -1.0, 1.0))
+            new_hab = Habitus(clamp(hab[i].val + lane_to_LR(pos[i].x, dir[i]) / (params.K + step[i].val), -1.0, 1.0))
             hab[i] = new_hab
         end
     end
