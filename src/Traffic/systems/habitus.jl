6be440d5988f2da8d78c16ad24086cc92c5812ce
theplatters <fscharnreitner@gmail.com>
2026-04-06 11:23:04 +0200

    rename for clarity

--- src/Traffic/systems/habitus.jl
+++ src/Traffic/systems/habitus.jl
@@ -1,19 +1,26 @@
-@inline lane_to_LR(laneIndex::Int, dir::Direction) =
+@inline relative_lane_sign(x::Int, dir::Direction) =
     dir == Clockwise ?
-    (laneIndex == 1 ? 1.0 : -1.0) :
-    (laneIndex == 2 ? 1.0 : -1.0)
+    (x == 1 ? 1.0 : -1.0) :
+    (x == 2 ? 1.0 : -1.0)
+
 
 function update_habitus!(world)
     params = Ark.get_resource(world, ModelParams)
 
     for (e, pos, dir, hab, step) in Query(world, (Position, Direction, Habitus, Step))
         @inbounds for i in eachindex(e)
-            new_hab = Habitus(clamp(hab[i].val + lane_to_LR(pos[i].x, dir[i]) / (params.K + step[i].val), -1.0, 1.0))
-            hab[i] = new_hab
+            hab[i] = Habitus(
+                clamp(
+                    hab[i].val + relative_lane_sign(pos[i].x, dir[i]) / (params.K + step[i].val),
+                    -1.0, 1.0
+                )
+            )
         end
     end
-    return
+
+    return nothing
 end
+
 
 function update_mean_habitus!(world)
     habitus = Ark.get_resource(world, MeanHabitus)
